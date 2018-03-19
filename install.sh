3a766f3f70c1f9c265f268b233ffe91dda7aee11
Alex Korotkikh <me@alexkorotkikh.com>
2018-03-19 13:00:41 +0100

    .tmux.conf & .gitignore

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES
 # Symlinks
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfv "$DOTFILES_DIR/runcom/.tmux.conf" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
